662462f20e5db74e679648b899c3ce716e2cb925
Fabricio Parola <fabricio.parola@gmail.com>
2020-05-15 01:50:00 -0300

    feat: throw `MissingParamError: email` when no email is provided

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,8 +1,19 @@
 export class SignupController {
   handle (httpRequest: any): any {
-    return {
-      status_code: 400,
-      body: new Error('MissingParamError: `name`')
+    const { body } = httpRequest
+
+    if (!body.name) {
+      return {
+        status_code: 400,
+        body: new Error('MissingParamError: `name`')
+      }
+    }
+
+    if (!body.email) {
+      return {
+        status_code: 400,
+        body: new Error('MissingParamError: `email`')
+      }
     }
   }
 }
