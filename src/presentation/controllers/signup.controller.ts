0d32ac50795c3d3db8ba562ea3cd0c74ab3bbbc6
Fabricio Parola <fabricio.parola@gmail.com>
2020-05-15 01:41:54 -0300

    feat: throw `MissingParamError: name` when no name is provided

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,7 +1,8 @@
 export class SignupController {
   handle (httpRequest: any): any {
     return {
-      status_code: 400
+      status_code: 400,
+      body: new Error('Missing param error: `name`')
     }
   }
 }
