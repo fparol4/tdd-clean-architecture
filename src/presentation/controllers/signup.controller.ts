42699d0532ed747cf5f1f9c15b2c0a406e0d4d40
Fabricio Parola <fabricio.parola@gmail.com>
2020-05-15 20:02:44 -0300

    refactor: change the error response to `HttpResponseHelper`

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,22 +1,17 @@
-import { HttpRequest, HttpResponse } from '../protocols/http.protocol'
 import MissingParamError from '../errors/missing-param.error'
+import HttpResponseHelper from '../helpers/http-response.helper'
+import { HttpRequest, HttpResponse } from '../protocols/http.protocol'
 
 export class SignupController {
   handle (httpRequest: HttpRequest): HttpResponse {
     const { body } = httpRequest
 
     if (!body.name) {
-      return {
-        status_code: 400,
-        body: new MissingParamError('name')
-      }
+      return HttpResponseHelper.badRequest(new MissingParamError('name'))
     }
 
     if (!body.email) {
-      return {
-        status_code: 400,
-        body: new MissingParamError('email')
-      }
+      return HttpResponseHelper.badRequest(new MissingParamError('email'))
     }
   }
 }
